638a5f6ba1a186fd0c641346e96677e600c7166e
Ben Gaull <brgaull1998@hotmail.com>
2020-10-12 17:28:52 -0700

    initial db dev commit

---
+++ Database-Dev/Utilities/Dataset-download-Windows.ps1
@@ -0,0 +1,29 @@
+$ProgressPreference = 'silentlyContinue'
+
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-01.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-01.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-01.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-02.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-02.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-02.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-02.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-02.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-03.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-03.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-03.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-03.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-04.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-04.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-04.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-04.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-05.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-05.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-05.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-05.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-06.csv
+Invoke-WebRequest https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-06.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2020-06.csv
+Invoke-WebRequest https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2020-06.csv
+
+$ProgressPreference = 'Continue'
+
