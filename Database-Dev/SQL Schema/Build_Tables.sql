a78b54082466dc45c0cafa8fe2935354ab366cc4
Ben Gaull <brgaull1998@hotmail.com>
2020-10-15 19:19:26 -0700

    adjusted order of table attributes to match the dataset to simplify data inserrtion, added missing attribute

--- Database-Dev/SQL Schema/Build_Tables.sql
+++ Database-Dev/SQL Schema/Build_Tables.sql
@@ -6,64 +6,73 @@ CREATE TABLE TaxiZones (
     PRIMARY KEY (locationID)
 );
 
+-- dispatching_base_num,pickup_datetime,dropoff_datetime,PULocationID,DOLocationID,SR_Flag
 CREATE TABLE FHV_Trip_Records (
-    PULocationID INT NOT NULL,
-    DOLocationID INT NOT NULL,
+    Dispatch_base_num INT,
     Pickup_datetime VARCHAR NOT NULL,
     Dropoff_datetime VARCHAR NOT NULL,
-    Dispatch_base_num INT,
+    PULocationID INT NOT NULL,
+    DOLocationID INT NOT NULL,
     SR_Flag VARCHAR,
     PRIMARY KEY (PULocationID, DOLocationID, Pickup_datetime, Dropoff_datetime)
 );
 
+-- hvfhs_license_num,dispatching_base_num,pickup_datetime,dropoff_datetime,PULocationID,DOLocationID,SR_Flag
 CREATE TABLE HV_FHV_Trip_Records (
-    PULocationID INT NOT NULL,
-    DOLocationID INT NOT NULL,
+    Hvfhs_license_num VARCHAR,
+    Dispatch_base_num INT,
     Pickup_datetime VARCHAR NOT NULL,
     Dropoff_datetime VARCHAR NOT NULL,
-    Dispatch_base_num INT,
+    PULocationID INT NOT NULL,
+    DOLocationID INT NOT NULL,
     SR_Flag VARCHAR,
-    Hvfhs_license_num VARCHAR,
     PRIMARY KEY (PULocationID, DOLocationID, Pickup_datetime, Dropoff_datetime)
 );
 
+-- VendorID,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,RatecodeID,store_and_fwd_flag,PULocationID,DOLocationID,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount,congestion_surcharge
 CREATE TABLE Yellow_Cab_Trip_Records (
-    PULocationID INT NOT NULL,
-    DOLocationID INT NOT NULL,
+    VendorID INT,
     tpep_pickup_datetime VARCHAR NOT NULL,
     tpep_dropoff_datetime VARCHAR NOT NULL,
-    Total_amount FLOAT,
-    RateCodeID INT,
+    Passenger_Count INT,
     Trip_Distance FLOAT,
-    VendorID INT,
+    RateCodeID INT,
     Store_and_fwd_flag VARCHAR,
+    PULocationID INT NOT NULL,
+    DOLocationID INT NOT NULL,
     Payment_type VARCHAR,
     Fare_ammount FLOAT,
     Extra VARCHAR,
     MTA_tax FLOAT,
-    Improvement_surcharge FLOAT,
     Tip_Amount FLOAT,
     Tolls_amount FLOAT,
+    Improvement_surcharge FLOAT,
+    Total_amount FLOAT,
+    congestion_surcharge FLOAT,
     PRIMARY KEY (PULocationID, DOLocationID, tpep_pickup_datetime, tpep_dropoff_datetime)
 );
 
+-- VendorID,lpep_pickup_datetime,lpep_dropoff_datetime,store_and_fwd_flag,RatecodeID,PULocationID,DOLocationID,passenger_count,trip_distance,fare_amount,extra,mta_tax,tip_amount,tolls_amount,ehail_fee,improvement_surcharge,total_amount,payment_type,trip_type,congestion_surcharge
 CREATE TABLE Green_Cab_Trip_Records (
-    PULocationID INT NOT NULL,
-    DOLocationID INT NOT NULL,
+    VendorID INT,
     tpep_pickup_datetime VARCHAR NOT NULL,
     tpep_dropoff_datetime VARCHAR NOT NULL,
-    Total_amount FLOAT,
-    RateCodeID INT,
-    Trip_didstance FLOAT,
-    VendorID INT,
     Store_and_fwd_flag VARCHAR,
-    Payment_Type VARCHAR,
-    Fare_Amount FLOAT,
+    RateCodeID INT,
+    PULocationID INT NOT NULL,
+    DOLocationID INT NOT NULL,
+    Passenger_Count INT,
+    Trip_didstance FLOAT,    
     Extra VARCHAR,
+    Fare_Amount FLOAT,
     MTA_tax FLOAT,
-    Improvement_surcharge FLOAT,
     Tip_amount FLOAT,
     Tolls_amount FLOAT,
+    ehail_fee FLOAT,
+    Improvement_surcharge FLOAT,
+    Total_amount FLOAT,
+    Payment_Type VARCHAR,
     Trip_type VARCHAR,
+    congestion_surcharge FLOAT,
     PRIMARY KEY (PULocationID, DOLocationID, tpep_pickup_datetime, tpep_dropoff_datetime)
 );
