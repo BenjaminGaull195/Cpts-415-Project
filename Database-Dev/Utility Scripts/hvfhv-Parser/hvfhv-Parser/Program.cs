4852575cba9a35af0351c76b724c11039b7bcb8e
Ben Gaull <brgaull1998@hotmail.com>
2020-10-16 18:49:34 -0700

    Updates c# parser

--- Database-Dev/Utility Scripts/hvfhv-Parser/hvfhv-Parser/Program.cs
+++ Database-Dev/Utility Scripts/hvfhv-Parser/hvfhv-Parser/Program.cs
@@ -23,14 +23,16 @@ namespace hvfhv_Parser
             //stopwatch to track execution time of parser
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            //output filestream
-            using (StreamWriter file = new StreamWriter(path_str + "\\hvfhv_insert.sql"))
-            {
 
-                
-                
-                // executes the following code for each file
-                foreach (FileInfo cur in files)
+            long tuple_count = 0;
+            int file_index = 0;
+            //output filestream
+           
+                               
+            // executes the following code for each file
+            foreach (FileInfo cur in files)
+            {
+                using (StreamWriter file = new StreamWriter(path_str + string.Format("\\hvfhv_insert_{0}.sql", file_index)))
                 {
                     //reads all lines from the file
                     string[] data = File.ReadAllLines(cur.FullName);
@@ -42,16 +44,15 @@ namespace hvfhv_Parser
                     {
                         //calles the parse function, then adds the parsed string the the output list
                         file.WriteLine(parse(entry, data_header.Length));
+                        ++tuple_count;
                     }
-
                 }
-
-                //write list of all formatted strings to sql file
-                //File.WriteAllLines(path_str + "\\hvfhv_insert.sql", parsed_data);
-
-
-                stopwatch.Stop();
+                ++file_index;
             }
+
+                
+            stopwatch.Stop();
+            
             TimeSpan timeSpan = stopwatch.Elapsed;
 
             // Format and display the TimeSpan value.
@@ -59,7 +60,7 @@ namespace hvfhv_Parser
                 timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
                 timeSpan.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
-
+            Console.WriteLine("Tuples : {0}", tuple_count);
 
         }
 
@@ -73,6 +74,13 @@ namespace hvfhv_Parser
             string[] parsed_data = csv.Split(',');
 
             //inserts parsed data into formatted sql string
+            /* Alternate Format Strings:
+             * FHV:         "INSERT INTO FHV_Trip_Records(Dispatch_base_num, Pickup_datetime, Dropoff_datetime, PULocationID, DOLocationID, SR_Flag) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\');", parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3], parsed_data[4], parsed_data[5]
+             * HV-FHV:      "INSERT INTO HV_FHV_Trip_Records(Hvfhs_license_num, Dispatch_base_num, Pickup_datetime, Dropoff_datetime, PULocationID, DOLocationID, SR_Flag) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\');", parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3], parsed_data[4], parsed_data[5], parsed_data[6]
+             * Yellow:      "INSERT INTO HV_FHV_Trip_Records(VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, Passenger_Count,Trip_Distance, RateCodeID, Store_and_fwd_flag, PULocationID, DOLocationID, Payment_type, Fare_ammount, Extra, MTA_tax, Tip_Amount, Tolls_amount, Improvement_surcharge, Total_amount, congestion_surcharge) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\', \'{7}\', \'{8}\', \'{9}\', \'{10}\', \'{11}\', \'{12}\', \'{13}\', \'{14}\', \'{15}\', \'{16}\', \'{17}\');", parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3], parsed_data[4], parsed_data[5], parsed_data[6], parsed_data[7], parsed_data[8], parsed_data[9],parsed_data[10], parsed_data[11], parsed_data[12], parsed_data[13], parsed_data[14], parsed_data[15], parsed_data[16], parsed_data[17]
+             * Green:       "INSERT INTO HV_FHV_Trip_Records(VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, Passenger_Count,Trip_Distance, RateCodeID, Store_and_fwd_flag, PULocationID, DOLocationID, Fare_ammount, Extra, MTA_tax, Tip_Amount, Tolls_amount, Improvement_surcharge, Total_amount, Payment_type, Trip_type, congestion_surcharge) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\', \'{7}\', \'{8}\', \'{9}\', \'{10}\', \'{11}\', \'{12}\', \'{13}\', \'{14}\', \'{15}\', \'{16}\', \'{17}\', \'{18}\');", parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3], parsed_data[4], parsed_data[5], parsed_data[6], parsed_data[7], parsed_data[8], parsed_data[9],parsed_data[10], parsed_data[11], parsed_data[12], parsed_data[13], parsed_data[14], parsed_data[15], parsed_data[16], parsed_data[17], parsed_data[18]
+             * Taxi Zones:  "INSERT INTO HV_FHV_Trip_Records() VALUES ();",
+            */
             sql_str = string.Format("INSERT INTO HV_FHV_Trip_Records(Hvfhs_license_num, Dispatch_base_num, Pickup_datetime, Dropoff_datetime, PULocationID, DOLocationID, SR_Flag) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\');", parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3], parsed_data[4], parsed_data[5], parsed_data[6]);
             
             //return formatted string
