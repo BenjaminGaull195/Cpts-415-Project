9989cad4ad98e30bf5b89084868ab1ea22316e4b
Kendrick Mitchell <32629467+KendrickMitchell@users.noreply.github.com>
2020-11-30 13:03:00 -0800

    Update Program.cs

--- Cpts-415-Taxi-Spark/Taxi-Spark-Algorithm/Program.cs
+++ Cpts-415-Taxi-Spark/Taxi-Spark-Algorithm/Program.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
-using Algorithms;
+
 
 //Formatted/Semi-structured IO
 using System.Xml;
@@ -25,7 +25,15 @@ using Microsoft.Spark.Sql;
 namespace Taxi_Spark_Algorithm
 {
 
-    
+    class NeighborData : IDisposable
+    {
+        public int attribute { get; set; }
+        public double distance { get; set; }
+        public void Dispose()
+        {
+
+        }
+    }
     
     class TaxiZoneData
     {
@@ -53,15 +61,9 @@ namespace Taxi_Spark_Algorithm
         static void Main(string[] args)
         {
             //Initialize Spark SQL session 
-            SparkSession spark = SparkSession
-                .Builder()
-                .AppName("Spark_Taxi_Hotspots")
-                .GetOrCreate();
-            DataFrame taxi_zones = spark.Read().Csv("");
-            DataFrame yellow_tripdata = spark.Read().Csv("");
-            DataFrame green_tripdata = spark.Read().Csv("");
-            DataFrame fhv_tripdata = spark.Read().Csv("");
-            DataFrame hvfhv_tripdata = spark.Read().Csv("");
+            SparkSession spark = new SparkSession();
+            Dataframe dataFrame = spark.read().Csv(input);
+
 
             //Build Dataset
                        
@@ -83,7 +85,7 @@ namespace Taxi_Spark_Algorithm
 
 
                 //Compute Getis-Ord Statistic and add z-score to output list
-                computationData.taxiZoneData.Add(new TaxiZoneData() { zoneID = i, zscore = Getis_Ord.Getis_Ord_Stat(neighbors)});
+                computationData.taxiZoneData.Add(new TaxiZoneData() { zoneID = i, zscore = Getis_Ord_Stat(neighbors)});
                 neighbors.Clear();
             }
 
@@ -96,7 +98,71 @@ namespace Taxi_Spark_Algorithm
         }
 
 
-        
+        //Initially created as serial algorithm 
+        //TODO: Add parallelization
+        public static double Getis_Ord_Stat(List<NeighborData> neighbors)
+        {
+            double _sum1 = 0, _sum2 = 0, _sum3 = 0, _X = 0, _S = 0;
+
+            /*
+            //Serial Version
+            //compute sum 1
+            double _sum1 = Sum1(ref neighbors);
+
+
+            //compute sum 2
+            double _sum2 = Sum2(ref neighbors);
+
+
+            //compute sum 3
+            double _sum3 = Sum3(ref neighbors);
+
+
+            //compute X
+            double _X = X(ref neighbors);
+
+
+            //compute S
+            double _S = S(ref neighbors, _X);
+            */
+            //Parallel Version
+
+            //TaskFactory to launch tasks, list of tasks to for task management, uses default TaskScheduler
+            List<Task<double>> tasks = new List<Task<double>>();
+            TaskFactory<double> factory = new TaskFactory<double>();
+
+            //start tasks for Sum1, SUm2, Sum3, X
+            Task<double> T = factory.StartNew(()=>Sum1(ref neighbors));
+            tasks.Add(T);
+
+            T = factory.StartNew(() => Sum2(ref neighbors));
+            tasks.Add(T);
+
+            T = factory.StartNew(() => Sum3(ref neighbors));
+            tasks.Add(T);
+
+            T = factory.StartNew(() => X(ref neighbors));
+            tasks.Add(T);
+
+            //wait for all tasks to complete
+            Task.WaitAll();
+
+            //get results from tasks
+            _sum1 = tasks[0].Result;
+            _sum2 = tasks[1].Result;
+            _sum3 = tasks[2].Result;
+            _X = tasks[3].Result;
+
+            //compute S
+            _S = S(ref neighbors, _X);
+
+            //compute stat and return
+            double zscore = (_sum1 - (_X * _sum2)) / (_S * Math.Sqrt((neighbors.Count * _sum3 - _sum2) / (neighbors.Count-1)));
+            return zscore;
+
+
+
+        }
         public static bool IsNeighbor(double radius, double x1, double y1, double x2, double y2)
         {
             x1 = Math.Pow((x1 - x2),2);
@@ -107,7 +173,65 @@ namespace Taxi_Spark_Algorithm
             return false;
         }//NeighborData and TaxiZonedata could be used to provide coordinates. A radius should be some constant in main or global
 
-        
+        private static double Sum1(ref List<NeighborData> neighbors)
+        {
+            double sum = 0;
+
+            foreach (NeighborData data in neighbors)
+            {
+                sum += data.distance * data.attribute;
+            }
+
+            return sum;
+        }
+
+        private static double Sum2(ref List<NeighborData> neighbors)
+        {
+            double sum = 0;
+
+            foreach (NeighborData data in neighbors)
+            {
+                sum += data.distance;
+            }
+
+            return sum;
+        }
+
+        private static double Sum3(ref List<NeighborData> neighbors)
+        {
+            double sum = 0;
+
+            foreach (NeighborData data in neighbors)
+            {
+                sum += (data.distance * data.distance);
+            }
+
+            return sum;
+        }
+
+        private static double X(ref List<NeighborData> neighbors)
+        {
+            double sum = 0;
+
+            foreach (NeighborData data in neighbors)
+            {
+                sum += data.attribute;
+            }
+
+            return sum / neighbors.Count;
+        }
+
+        private static double S(ref List<NeighborData> neighbors, double X)
+        {
+            double sum = 0;
+
+            foreach (NeighborData data in neighbors)
+            {
+                sum += (data.attribute * data.attribute);
+            }
+
+            return Math.Sqrt((sum/neighbors.Count) - Math.Pow(X, 2));
+        }
 
         /*
         private static void BuildNeighborTable(int num_zones, double radius, SparkContext C){//We require a table detailing a neighboring regions in the DB.
@@ -132,7 +256,8 @@ namespace Taxi_Spark_Algorithm
                         //In a nested loop compare coordinates with all other regions which you also pull from the taxi zone table
                     }
                 }
-        }//This function is intended to precompute neighbor relations which can be easily retrieved as distinct neighborhood lists given the current_id. These lists can be summed over to get the G* stat*/
+        }//This function is intended to precompute neighbor relations which can be easily retrieved as distinct neighborhood lists given the current_id. These lists can be summed over to get the G* stat
+        */
 
 
     }
@@ -146,30 +271,30 @@ namespace Taxi_Spark_Algorithm
 SELECT zoneID, cal_date, Count(*)
 FROM(
 
-SELECT * 
+SELECT zoneID,cal_id,trip_id 
 FROM YELLOW
-WHERE ((pickup_zone = x OR dropoff_zone = x) AND (pickup_date = y OR dropoff_date = y))
+
 
 UNION
 
-SELECT * 
+SELECT  zoneID,cal_id,trip_id 
 FROM GREEN
-WHERE ((pickup_zone = x OR dropoff_zone = x) AND (pickup_date = y OR dropoff_date = y))
+
 
 UNION
 
-SELECT * 
+SELECT  zoneID,cal_id,trip_id 
 FROM HVF
-WHERE ((pickup_zone = x OR dropoff_zone = x) AND (pickup_date = y OR dropoff_date = y))
+
 
 UNION
 
-SELECT * 
+SELECT  zoneID,cal_id,trip_id 
 FROM HVFV
-WHERE ((pickup_zone = x OR dropoff_zone = x) AND (pickup_date = y OR dropoff_date = y))
+
 
 )
 GROUP BY zoneID, cal_date 
 
-//We need to split the time values apart with the calendar date and daily clock time separated.
+//We need to split the time values apart with the calendar date and daily clock time separated if we want to do something like this.
 */
