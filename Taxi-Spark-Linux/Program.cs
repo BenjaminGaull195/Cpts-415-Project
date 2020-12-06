b0e7fa3f488202f3291cda02dc51001080737d67
Ubuntu <ubuntu@ip-172-31-32-70.us-east-2.compute.internal>
2020-12-06 23:35:59 +0000

    added new dotnet project beccause the current one is having issues when i build it on the VM

---
+++ Taxi-Spark-Linux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taxi_Spark_Linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
